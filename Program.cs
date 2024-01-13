6ea9db286a467274b3740c85d458c2f6042a7a8e
David Smith <cpmpdavidsmith@icloud.com>
2024-01-13 15:55:42 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Queue ar = new Queue();
+
+            ar.Enqueue("red");
+            ar.Enqueue("roses");
+            ar.Enqueue("wine");
+
+            Console.WriteLine("Does the que contain roses? True or false? " + ar.Contains("roses"));
+            Console.Read();
+        }
+    }
+}
